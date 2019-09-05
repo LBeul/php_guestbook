2fa2dd2db2c5a5a9c0db8c5d8d32bacc2ecca4dc
Bartosz <bartek.burgiel@hotmail.com>
2019-09-05 16:25:47 +0200

    initialize admin.php page

---
+++ src/admin.php
@@ -0,0 +1,35 @@
+<!--this page manages contents of the guestbook table-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin-Verwaltung</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php
+		if (!isset($_POST["submit"])) {
+
+		echo "
+			<h1>Willkommen in der Admin Seite des Gästebuches</h1>
+			<p>Bitte geben Sie das Adminpasswort ein<p>
+
+			<form method='POST'>
+				<input type='password' name='password'/>
+				<input type='submit' name='submit'/> 
+			<form/>
+			";
+		} else {
+			$password = $_POST['password'];
+			
+			//if wrong password
+			if ($password != "root") {
+				echo "Falsches Passwort";
+			} else {
+				echo "Richtiges Passwort";
+			}
+		}
+
+	?>
+</body>
+</html>
