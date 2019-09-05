4c0e0e0aa9c0c2cbf345342b9077c28147124fc5
Bartosz <bartek.burgiel@hotmail.com>
2019-09-05 16:06:38 +0200

    add initDb.sql

---
+++ initDb.sql
@@ -0,0 +1,10 @@
+# here you go :*
+# muesste so funktioneren
+
+CREATE TABLE guestbook (
+	firstName VARCHAR(256),
+	lastName VARCHAR(256),
+	userEmail VARCHAR(256),
+	userEntry MEDIUMTEXT, 
+	entryDate TIMESTAMP
+);
