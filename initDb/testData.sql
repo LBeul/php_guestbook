b8c13cfa830bf8cc3dc49affe77e666a5229f2f6
Bartosz <bartek.burgiel@hotmail.com>
2019-09-06 11:35:05 +0200

    add few more test queries

--- initDb/testData.sql
+++ initDb/testData.sql
@@ -31,3 +31,31 @@ INSERT INTO guestbookEntry VALUES (
 	"2019-09-06 09:42:42"
 ) 
 ;
+
+INSERT INTO guestbookEntry VALUES (
+	"Das Gästebuch funktioniert wirklich einwandfrei!",
+	"321tset",
+	"2019-09-06 11:32:53"
+)
+;
+
+INSERT INTO guestbookEntry VALUES (
+	"Kann nur empfehlen 10/10",
+	"123test",
+	"2019-09-06 11:29:13"
+)
+;
+
+INSERT INTO guestbookEntry VALUES (
+	"Ich nutze das Physikunterricht produktiv aus",
+	"123test",
+	"2019-09-06 11:30:31"
+)
+;
+
+INSERT INTO guestbookEntry VALUES (
+	"Daniel sitzt neben mir und Katharina trägt vor",
+	"123test",
+	"2019-09-06 11:34:42"
+)
+;
