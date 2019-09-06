87da42ae482c8cf38e58975227b1ae5e7c5723ee
Bartosz <bartek.burgiel@hotmail.com>
2019-09-06 20:00:01 +0200

    update admin.php page

--- src/admin.php
+++ src/admin.php
@@ -43,120 +43,95 @@
 			}
 		}
 
-		if(!isset($_POST["submit"])) 
-		{
+		if(isset($_POST["submit"])) {
 
-			echo "
-				<h1>Willkommen in der Admin Seite des Gästebuches</h1>
-				<p>Bitte geben Sie das Adminpasswort ein<p>
+			// Database connection
+			$dbConnection = mysqli_connect("localhost", "root", "", "bbs");
 
-			<form method='POST'>
-					<input type='password' name='password'/>
-					<input type='submit' name='submit'/> 
-			<form/>";
-
-		} 
-		else 
-		{
-			$password = $_POST['password'];
+			// Querry
+			$dbSelectAllUsers = "SELECT 
+									guestbookuser.firstName AS firstName,
+									guestbookUser.lastName AS lastName,
+									guestbookUser.userEmail AS userEmail,
+									guestbookUser.userEntryKey AS entryKey,
+									guestbookUser.password AS userPassword
+									FROM guestbookUser ;";
 			
-			// If password is wrong
-			if($password != "root") 
-			{
-				echo "Falsches Passwort";
-			}
-			else 
-			{
-				// This array stores all entries as objects 
-				$allEntries = array();
-				
-				// Database connection
-				$dbConnection = mysqli_connect("localhost", "root", "", "bbs");
+			$users = mysqli_query($dbConnection, $dbSelectAllUsers);
+			echo mysqli_error($dbConnection);
 
-				// Querry
-				$dbSelectAllUsers = "SELECT 
-										guestbookuser.firstName AS firstName,
-										guestbookUser.lastName AS lastName,
-										guestbookUser.userEmail AS userEmail,
-										guestbookUser.userEntryKey AS entryKey,
-										guestbookUser.password AS userPassword
-										FROM guestbookUser ;";
-				
-				$users = mysqli_query($dbConnection, $dbSelectAllUsers);
+			while($data = mysqli_fetch_assoc($users)) {	
+
+				// Temporarily store data 
+				$fullName = $data['firstName']." ".$data['lastName'];
+				$userEmail = $data['userEmail'];
+				$userKey = $data['entryKey'];
+				$userPassword = $data['userPassword'];
+
+				// Print user info
+				echo "<h1>$fullName</h1>";
+				echo "<ul>
+						<li>".bold("E-Mail").": <a href=mailto:$userEmail>$userEmail</a></li>
+						<li>".bold("Key").": $userKey</li>
+						<li>".bold("Schlüssel").": $userPassword</li>
+					</ul>";
+
+				// Print all user's entries
+
+				$dbSelectAllEntries = "SELECT * FROM guestbookEntry
+										WHERE userEntryKey = '$userKey' ;";
+
+
+
+				// Get all entries
+				$entries = mysqli_query($dbConnection, $dbSelectAllEntries);
 				echo mysqli_error($dbConnection);
 
-				while($data = mysqli_fetch_assoc($users)) {	
+				// Open table
+				echo "<div>
+					<table border>";
 
-					// Temporarily store data 
-					$fullName = $data['firstName']." ".$data['lastName'];
-					$userEmail = $data['userEmail'];
-					$userKey = $data['entryKey'];
-					$userPassword = $data['userPassword'];
+				// Table head 
+				echo "<tr>";
+					echo toTd(bold("Eintrag"));
+					echo toTd(bold("Datum"));
+					echo toTd(bold("ID"));
+					echo toTd(bold("Action"));
+				echo "</tr>";
 
-					// Print user info
-					echo "<h1>$fullName</h1>";
-					echo "<ul>
-							<li>".bold("E-Mail").": <a href=mailto:$userEmail>$userEmail</a></li>
-							<li>".bold("Key").": $userKey</li>
-							<li>".bold("Schlüssel").": $userPassword</li>
-						</ul>";
+				while ($entry = mysqli_fetch_assoc($entries)) {
+					// Temporarily store all variables
+					$entryText = $entry['entry'];
+					$entryDate = $entry['entryDate'];
+					$entryID = $entry['ID'];
 
-					// Print all user's entries
-
-					$dbSelectAllEntries = "SELECT * FROM guestbookEntry
-											WHERE userEntryKey = '$userKey' ;";
-
-
-
-					// Get all entries
-					$entries = mysqli_query($dbConnection, $dbSelectAllEntries);
-					echo mysqli_error($dbConnection);
-
-					// Open table
-					echo "<div>
-						<table border>";
-
-					// Table head 
+					// TODO make it pretty :)
+					// Maybe grid or smth
 					echo "<tr>";
-						echo toTd(bold("Eintrag"));
-						echo toTd(bold("Datum"));
-						echo toTd(bold("ID"));
-						echo toTd(bold("Action"));
+						echo toTd($entryText);
+						echo toTd($entryDate);
+						echo toTd($entryID);
+
+						echo "<td>
+								<form action='admin.php' method='post'>
+									<input type='submit' name=$entryID value='Löschen' />
+								</form>
+							</td>";
+
+							// If button clicked 
+							if (isset($_POST[$entryID])) {
+								deleteButton($entryID);
+							}
+
 					echo "</tr>";
-
-					while ($entry = mysqli_fetch_assoc($entries)) {
-						// Temporarily store all variables
-						$entryText = $entry['entry'];
-						$entryDate = $entry['entryDate'];
-						$entryID = $entry['ID'];
-
-						// TODO make it pretty :)
-						// Maybe grid or smth
-						echo "<tr>";
-							echo toTd($entryText);
-							echo toTd($entryDate);
-							echo toTd($entryID);
-
-							echo "<td>
-									<form action='admin.php' method='post'>
-										<input type='submit' name=$entryID value='Löschen' />
-									</form>
-								</td>";
-
-								// If button clicked 
-								if (isset($_POST[$entryID])) {
-									deleteButton($entryID);
-								}
-
-						echo "</tr>";
-					}
-					// Close table
-					echo "
-							</table>
-						</div>";
 				}
+				// Close table
+				echo "
+						</table>
+					</div>";
 			}
 		}
+	
 
 	?>
 </body>
