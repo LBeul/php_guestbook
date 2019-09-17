88ab95555ddec802df3239c48253e4cbadd12783
LBeul <l.beul@outlook.de>
2019-09-17 21:55:06 +0200

    Styled adminLogin

--- src/adminLogin.php
+++ src/adminLogin.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>TF Admin LogIn</title>
+	<title>TF Lehrer LogIn</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+	<link rel="stylesheet" type="text/css" href="../css/adminForm.css">
+	<link rel="stylesheet" type="text/css" href="../css/error.css">
 </head>
 <body>
+	<div class="container">
+		<h1 id='logo'>TeacherFeedback</h1>
+
 	<?php
 		// Clear session
 		if (session_status() == PHP_SESSION_ACTIVE) {
@@ -14,13 +20,17 @@
 		if(!isset($_POST["submit"])) {
 
 			echo "
-				<h1>Willkommen in der Admin Seite des Gästebuches</h1>
-				<p>Bitte geben Sie das Adminpasswort ein<p>
-
-			<form method='POST'>
-					<input type='password' name='password'/>
-					<input type='submit' name='submit'/> 
-			<form/>";
+				<h2>Willkommen auf der Lehrer-Seite!</h2>
+				<p>
+					Sind Sie schon gespannt, was Ihre Schüler so über Sie denken? In wenigen Sekunden
+					wird dieses Geheimnis gelüftet - geben Sie lediglich Ihr Passwort ein, um die Liste
+					der Feedback-Einträge zu erhalten.
+				</p>
+				<form method='POST'>
+					<label for='password'>Passwort</label>
+					<input type='password' name='password' required/>
+					<input type='submit' name='submit' value='Einloggen'/> 
+				<form/>";
 
 		} else {
 			
@@ -33,13 +43,17 @@
 
 			// If password is wrong
 			if($password != "root") {
-				echo "<h1>Falsches Passwort</h1>";
-				echo"<a href='./adminLogin.php'>Zurück zum Admin Login</a>";
+				echo "
+				<div class='error'>
+					Hoppla, das war leider ein falsches Passwort!<br/>
+					<a class='retry-link' href='./adminLogin.php'>Zurück zum Admin Login</a>
+				</div>";
 			} else {
 				header("Location: admin.php");
 			}
 		}
 
 		?>
+	</div>
 </body>
 </html>
